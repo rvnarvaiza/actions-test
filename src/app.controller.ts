9d0507e0e7ac1bdcf5e2127151606b5525f737d1
rvnarvaiza <rvnarvaiza@gmail.com>
2023-03-10 13:59:58 +0800

    added function that returns sum of two numbers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  getSum(
+    @Query('num_1', ParseIntPipe) num1: number,
+    @Query('num_2', ParseIntPipe) num2: number,
+  ) {
+    return num1 + num2;
   }
 }
